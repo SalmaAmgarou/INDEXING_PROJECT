ce253817c31b97c08a70a07681d480891ba9eabc
hafdaoui <lilpump0629@gmail.com>
2024-03-08 14:06:48 +0100

    test

--- test/domain/test_hash.py
+++ test/domain/test_hash.py
@@ -9,10 +9,10 @@ class TestHash(unittest.TestCase):
         self.hash_object = Hash()
 
     def test_should_return_key_mod(self):
-        self.assertEqual(self.hash_object.hash_function(24), 1, "Wrong mod key")
+        self.assertEqual(self.hash_object.hash_function(24), 1, "Wrong mod key!")
 
     def test_should_insert_key_on_address(self):
-        self.assertEqual(self.hash_object.insert_key(24, 'Element'), 1, "Wrong address")
+        self.assertEqual(self.hash_object.insert_key(24, 'Element'), 1, "Wrong address!")
 
     def test_should_insert_key_on_next_address(self):
-        self.assertEqual(self.hash_object.insert_key(48, 'Element'), 2, "Wrong address")
+        self.assertEqual(self.hash_object.insert_key(48, 'Element'), 2, "Wrong address!")
